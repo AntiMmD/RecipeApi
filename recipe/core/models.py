bb19e45dcf7981aa0c94b43bb074fb2cb4e338cd
IcePack <mhmd.zayeri@gmail.com>
2026-04-22 21:31:20 +0330

    Add custom user

--- recipe/core/models.py
+++ recipe/core/models.py
@@ -7,12 +7,24 @@ from django.contrib.auth.models import(
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None,**extra_fields):
+        if not email:
+            raise ValueError('User must have an email')
+        email = self.normalize_email(email).lower()
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using= self._db)
 
         return user
-    
+
+    def create_superuser(self, email, password, **extra_fields):
+        user = self.create_user(email, password, **extra_fields)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=225, unique=True)
--- recipe/core/tests/test_models.py
+++ recipe/core/tests/test_models.py
@@ -12,3 +12,26 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_with_email_normalized(self):
+        sample_emails = [
+            ['test1@EXAMPLE.COM', 'test1@example.com'],
+            ['test2@EXAMPLE.com', 'test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'test3@example.com']
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'sample123')
+
+    def test_create_super_user(self):
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test1234'
+        )
+
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
