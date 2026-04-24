be99d289c0a8f64f191f08b49ea76fb19ad544a6
IcePack <mhmd.zayeri@gmail.com>
2026-04-24 23:08:48 +0330

    Add custom admin model

---
+++ recipe/core/tests/test_admin.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+class AdminSiteTest(TestCase):
+    
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email = 'admin@exmaple.com',
+            password = 'testpass123'
+        )
+        self.client.force_login(self.admin_user)
+
+        self.user = get_user_model().objects.create_user(
+            email = 'user@example.com',
+            password='testpass1234',
+            name = 'Test User'
+        )
+
+    def test_user_list(self):
+        url = reverse('admin:core_user_changelist')
+        response = self.client.get(url)
+
+        self.assertContains(response, self.user.name)
+        self.assertContains(response, self.user.email)
+
+    def test_edit_user_page(self):
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+    
+    def test_create_user_page(self):
+        url = reverse('admin:core_user_add')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
--- recipe/recipe/settings.py
+++ recipe/recipe/settings.py
@@ -117,3 +117,4 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 AUTH_USER_MODEL = 'core.User'
+# LANGUAGE_CODE = 'fa'
